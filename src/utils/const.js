39e6d577bff5a48ac29fb74f33c723215583534f
Nico Prananta <nicnocquee@users.noreply.github.com>
2021-03-20 11:32:56 +0100

    update registration URL

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,2 +1,2 @@
 export const EventRegistrationURL =
-  "https://github.com/nicnocquee/swift-study-group-indonesia/discussions/2";
+  "https://github.com/nicnocquee/swift-study-group-indonesia/discussions";
